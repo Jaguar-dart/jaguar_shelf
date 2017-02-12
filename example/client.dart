ec7fa875ece7c80344d7af29375249178390146a
Ravi Teja Gudapati <tejainece@gmail.com>
2017-02-12 11:23:01 +0100

    Added client for example

---
+++ example/client.dart
@@ -0,0 +1,34 @@
+library jaguar_shelf.example.simple.client;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const String kHostname = 'localhost';
+
+const int kPort = 8080;
+
+final http.Client _client = new http.Client();
+
+Future<Null> printHttpClientResponse(http.Response resp) async {
+  print('=========================');
+  print("body:");
+  print(resp.body);
+  print("statusCode:");
+  print(resp.statusCode);
+  print("headers:");
+  print(resp.headers);
+  print('=========================');
+}
+
+Future<Null> execSomeRoute() async {
+  String url = "http://$kHostname:$kPort/some/route";
+  http.Response resp = await _client.get(url);
+
+  printHttpClientResponse(resp);
+}
+
+main() async {
+  await execSomeRoute();
+}
