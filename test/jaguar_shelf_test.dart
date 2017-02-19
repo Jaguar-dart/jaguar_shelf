a05c19fc5783e03a546e196a40453c13874e6056
Ravi Teja Gudapati <tejainece@gmail.com>
2017-02-19 10:19:25 +0100

    Upgraded to Jaguar 0.4.x

--- test/jaguar_shelf_test.dart
+++ test/jaguar_shelf_test.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2017, teja. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:jaguar_shelf/jaguar_shelf.dart';
+//TODO import 'package:jaguar_shelf/jaguar_shelf.dart';
 import 'package:test/test.dart';
 
 void main() {
